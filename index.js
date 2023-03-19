94cb8d06c1b95bcce84c9aa5a17099a795719e53
Willian-Louback <williandlouback@gmail.com>
2023-03-19 02:15:25 -0300

    quase funcional

--- index.js
+++ index.js
@@ -57,6 +57,9 @@ class Tabuleiro{
         this.branco = branco;
         this.verde = verde;
         this.positionVerdes = [];
+        this.cases = [];
+        this.wave = 0;
+        this.quantidade = 0;
         //this.matriz = this.criarMatriz();
     }
 
@@ -71,15 +74,25 @@ class Tabuleiro{
             const atribuirValores = linhas[i].trim().split(' ').map(Number);
             matriz[i] = atribuirValores;
         }
-        matriz[0][1] = 1;
+        //matriz[0][1] = 1;
+        /*matriz[0][2] = 1;
+        matriz[1][1] = 1;
+        matriz[1][0] = 1;
+        matriz[1][2] = 1;*/
+        /*matriz[1][2] = 1;
+        matriz[1][3] = 1;
+        matriz[1][3] = 1;
+        matriz[1][4] = 1;*/
         this.verificaPosicoes(matriz);
     }
 
     verificaPosicoes = (matriz) => {
-        matriz.forEach((arrays, indiceArray) =>{
-            arrays.forEach((numbers, indice) => {
+        this.positionVerdes = [];
+
+        //matriz.forEach((arrays, indiceArray) =>{
+            /*arrays.forEach((numbers, indice) => {
             
-                /*if(indiceArray == 0 && indice == 1){
+                if(indiceArray == 0 && indice == 1){
                     if(
                         matriz[indiceArray][indice+1] == this.verde &&
                         matriz[indiceArray+1][indice] == this.verde &&
@@ -87,18 +100,101 @@ class Tabuleiro{
                     ){
 
                     }
-                }*/
+                }
                 if(numbers == this.verde){
-                    this.positionVerdes.push(["Array:",indiceArray, "indice:",indice]);
+                    this.positionVerdes.push(["Array:", indiceArray, "indice:", indice]);
                 }
             })
-        })
+        })*/
 
-        console.log(this.positionVerdes);
+        this.adicionarPosicao(matriz);
     }
 
-    adicionarPosicao(){
+    adicionarPosicao(matriz){
+        matriz.forEach((arrays, indiceArray) => {
+            arrays.forEach((numbers, indice) => {
+                if(indiceArray == 0 && indice == 1){ //cuidando do canto superior esquerdo
+                    this.cases = [
+                        matriz[indiceArray][indice+1],
+                        matriz[indiceArray+1][indice],
+                        matriz[indiceArray+1][indice-1],
+                        matriz[indiceArray+1][indice+1]
+                    ]
 
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indiceArray == 0 && indice != 1 && indice != 0 && indice != 84){ // cuidando do topo
+                    this.cases = [
+                        matriz[indiceArray][indice+1],
+                        matriz[indiceArray][indice-1],
+                        matriz[indiceArray+1][indice],
+                        matriz[indiceArray+1][indice-1],
+                        matriz[indiceArray+1][indice+1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indiceArray == 0 && indice == 84){ //cuidando do canto superior direito
+                    this.cases = [
+                        matriz[indiceArray][indice-1],
+                        matriz[indiceArray+1][indice],
+                        matriz[indiceArray+1][indice-1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indiceArray == 64 && indice == 0){ //cuidando do canto inferior esquerdo
+                    this.cases = [
+                        matriz[indiceArray-1][indice],
+                        matriz[indiceArray-1][indice+1],
+                        matriz[indiceArray][indice+1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indiceArray == 64 && indice == 83){ //cuidando do canto inferior direito
+                    this.cases = [
+                        matriz[indiceArray-1][indice],
+                        matriz[indiceArray-1][indice-1],
+                        matriz[indiceArray-1][indice+1],
+                        matriz[indiceArray][indice-1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indiceArray == 64 && indice == 83){ //cuidando da parte de baixo
+                    this.cases = [
+                        matriz[indiceArray-1][indice],
+                        matriz[indiceArray-1][indice-1],
+                        matriz[indiceArray-1][indice+1],
+                        matriz[indiceArray][indice-1],
+                        matriz[indiceArray][indice+1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                }
+
+                if(matriz[indiceArray][indice] == this.branco){ //Conferindo se é branco ou verde
+                    this.quantidade > 1 && this.quantidade < 5 ? matriz[indiceArray][indice] = this.verde : null;
+                    //console.log('era branco:',matriz[indiceArray][indice]);
+                } else if(matriz[indiceArray][indice] == this.verde){
+                    this.quantidade > 3 && this.quantidade < 6 ? null : matriz[indiceArray][indice] = this.branco;
+                    //console.log('era verde:',matriz[indiceArray][indice]);
+                }
+
+                this.cases = [];
+                this.quantidade = 0;
+            })
+        })
+        
+        this.wave++;
+        console.log("Wave:",this.wave);
+        if(this.wave == 10){
+            return;
+        }
+        console.log(matriz[0]);
+        this.verificaPosicoes(matriz);
     }
 }
 
