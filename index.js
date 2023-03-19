a17e985221630afcc4d9c22ca38d8426da05dd0e
Willian-Louback <williandlouback@gmail.com>
2023-03-19 02:45:09 -0300

    jogo pronto

--- index.js
+++ index.js
@@ -1,66 +1,12 @@
 const fs = require('fs');
 
-/*class Tabuleiro{
-    constructor(x, y, inicio, destino){ //x linha; y coluna;
-        this.x = x;
-        this.y = y;
-        this.inicio = inicio;
-        this.destino = destino;
-        this.branco = 0;
-        this.verde = 1;
-        this.casas = [];
-        this.casaDestino = this.x*this.y;
-        this.casaInicio = 1;
-    }
-
-    gerarCasas(){
-        let casas = this.casas;
-        for(let i = 1; i <= this.x * this.y; i++){
-            if(i == this.casaInicio){
-                casas.push(this.inicio);
-            } else if(i == this.casaDestino){
-                casas.push(this.destino);
-                console.log(i)
-            } else{
-                casas.push(this.branco);
-            }
-        }
-        this.atualizarPosicoes(casas);
-    }
-
-    atualizarPosicoes(casas){
-        casas.forEach((valor, indice) => {
-            if((indice+1) == 55){
-                casas.splice(indice, 1, this.verde);
-            }
-        });
-        this.testeDados();
-    }
-    
-    testeDados(){
-        console.log(this.x * this.y, this.inicio, this.destino);
-        console.log("cores:",this.branco, this.verde);
-        console.log('Array:', this.casas);
-        console.log("Tamanho:", this.casas.length);
-        //this.casas.forEach(casas =>{
-        //    console.log(casas);
-        //})
-    }
-}
-
-//const tabuleiro = new Tabuleiro(65, 85, 3, 4);
-const tabuleiro = new Tabuleiro(7, 8, 3, 4);
-
-//tabuleiro.gerarCasas();*/
 class Tabuleiro{
     constructor(branco, verde){
         this.branco = branco;
         this.verde = verde;
-        this.positionVerdes = [];
         this.cases = [];
         this.wave = 0;
         this.quantidade = 0;
-        //this.matriz = this.criarMatriz();
     }
 
     criarMatriz = () => {
@@ -87,26 +33,6 @@ class Tabuleiro{
     }
 
     verificaPosicoes = (matriz) => {
-        this.positionVerdes = [];
-
-        //matriz.forEach((arrays, indiceArray) =>{
-            /*arrays.forEach((numbers, indice) => {
-            
-                if(indiceArray == 0 && indice == 1){
-                    if(
-                        matriz[indiceArray][indice+1] == this.verde &&
-                        matriz[indiceArray+1][indice] == this.verde &&
-                        matriz[indiceArray+1][indice-1] == this.verde 
-                    ){
-
-                    }
-                }
-                if(numbers == this.verde){
-                    this.positionVerdes.push(["Array:", indiceArray, "indice:", indice]);
-                }
-            })
-        })*/
-
         this.adicionarPosicao(matriz);
     }
 
@@ -123,7 +49,7 @@ class Tabuleiro{
 
                     this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
                     //console.log(this.quantidade);
-                } else if(indiceArray == 0 && indice != 1 && indice != 0 && indice != 84){ // cuidando do topo
+                } else if(indiceArray == 0 && indice != 0 && indice != 84){ // cuidando do topo
                     this.cases = [
                         matriz[indiceArray][indice+1],
                         matriz[indiceArray][indice-1],
@@ -162,7 +88,7 @@ class Tabuleiro{
 
                     this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
                     //console.log(this.quantidade);
-                } else if(indiceArray == 64 && indice == 83){ //cuidando da parte de baixo
+                } else if(indiceArray == 64 && indice != 84){ //cuidando da parte de baixo
                     this.cases = [
                         matriz[indiceArray-1][indice],
                         matriz[indiceArray-1][indice-1],
@@ -173,7 +99,43 @@ class Tabuleiro{
 
                     this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
                     //console.log(this.quantidade);
-                }
+                } else if(indice == 0 && indiceArray != 0){ //cuidando do canto esquerdo
+                    this.cases = [
+                        matriz[indiceArray-1][indice],
+                        matriz[indiceArray-1][indice+1],
+                        matriz[indiceArray+1][indice],
+                        matriz[indiceArray+1][indice+1],
+                        matriz[indiceArray][indice+1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indice == 84 && indiceArray != 64){ //cuidando do canto direito
+                    this.cases = [
+                        matriz[indiceArray-1][indice],
+                        matriz[indiceArray-1][indice-1],
+                        matriz[indiceArray+1][indice],
+                        matriz[indiceArray+1][indice-1],
+                        matriz[indiceArray][indice-1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } else if(indiceArray != 0 && indiceArray != 64 && indice != 0 && indice != 84){ //restante (Arrumar depois, acho que posso remover algumas condições)
+                    this.cases = [
+                        matriz[indiceArray][indice+1],
+                        matriz[indiceArray][indice-1],
+                        matriz[indiceArray-1][indice],
+                        matriz[indiceArray+1][indice],
+                        matriz[indiceArray-1][indice+1],
+                        matriz[indiceArray-1][indice-1],
+                        matriz[indiceArray+1][indice-1],
+                        matriz[indiceArray+1][indice+1]
+                    ]
+
+                    this.cases.forEach(cases => cases == this.verde ? this.quantidade++ : null);
+                    //console.log(this.quantidade);
+                } 
 
                 if(matriz[indiceArray][indice] == this.branco){ //Conferindo se é branco ou verde
                     this.quantidade > 1 && this.quantidade < 5 ? matriz[indiceArray][indice] = this.verde : null;
